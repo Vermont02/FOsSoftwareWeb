1bb0bdb8cbe485ced2f6b0fc6ea6c56a87bbf2d2
Vermont02 <bquesenbm@gmail.com>
2025-02-20 21:24:35 -0500

    A

--- more-pages.js
+++ more-pages.js
@@ -22,28 +22,48 @@ try {
     
     unsup();
 
-    const win = document.getElementById('winds');
+dragElement(document.getElementById("wind"));
 
-    win.onmousedown = function(aero) {
-      let offsetX = aero.clientX - win.getBoundingClientRect().left;
-      let offsetY = aero.clientY - win.getBoundingClientRect().top;
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+  
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
 
-      function moveWith(aero) {
-        win.style.left = aero.clientX - offsetX + 'px';
-        win.style.top = aero.clientY - offsetY + 'px';
-      }
+    elmnt.onmousedown = dragMouseDown;
+  }
 
-      document.addEventListener('mousemove', moveWith);
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
 
-      win.onmouseup = function() {
-        document.removeEventListener('mousemove', moveWith);
-        win.onmouseup = null;
-      };
-    };
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+ 
+    document.onmousemove = elementDrag;
+  }
 
-    win.ondragstart = function() {
-      return false;
-    };
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 } catch (wrongdoing) {
     console.log('error in "more-pages.js":', wrongdoing)
