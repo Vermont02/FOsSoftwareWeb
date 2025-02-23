420b84b407ca4265c23aa6e857f143fbb1cbf5ef
Vermont02 <bquesenbm@gmail.com>
2025-02-23 17:49:53 -0500

    S

--- more-pages.js
+++ more-pages.js
@@ -4,16 +4,22 @@ try {
 
     // Logs when HTML DOM is fully loaded
     document.addEventListener('DOMContentLoaded', function() {
+      // Logs:
         console.log('HTML DOM Loaded At...');
         console.log(Date());
+        // Plays sound linked above
         correctsound.play();
-        dragElement(document.getElementById("mydiv"));
+        // Loads window when DOM loads
+        dragElement(document.getElementById("window"));
     });
 
+    // Detects if user is using mobile 
     function mobileDetect() {
         return /iPhone|iPad|Mobi|Android/i.test(navigator.userAgent);
     }
 
+    /* Alerts the mobile user that they should buy a computer.
+    it doesn't let them view the site by executing an alert loop (a poorly coded one) */
     function unsup() {
         if (mobileDetect()) {
             alert('This site is unsupported on your device.');
@@ -23,45 +29,46 @@ try {
     
     unsup();
 
-    // Make the DIV element draggable:
-dragElement(document.getElementById("mydiv"));
+    // Make the DIV "window" draggable:
+dragElement(document.getElementById("winodw"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
+    // If present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
+    // Otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
+    // Get the mouse cursor position at load:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
+    // Call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
+    // Calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
+    // Set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
-    // stop moving when mouse button is released:
+    /* Stops moving when mouse button is released 
+    (why you stop moving your mouse eh? pick the up goddamn mouse!!) */
     document.onmouseup = null;
     document.onmousemove = null;
   }
