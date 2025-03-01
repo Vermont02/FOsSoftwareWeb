16405a30770c4075e36aebc0e0ebcfcceb3f3080
Vermont02 <bquesenbm@gmail.com>
2025-03-01 15:20:15 -0500

    new page added

---
+++ term.js
@@ -0,0 +1,34 @@
+let input = document.getElementById('terminput');
+let output = document.getElementById('termoutput');
+
+input.addEventListener('keydown', function(e) {
+  if (e.key === "Enter") {
+      commands();
+  }
+});
+
+function commands() {
+  if (input.value === "--help" || input.value === "--h") {
+     hcom();
+  } else {
+  err();
+  }
+};
+
+function err() {
+  let disp = input.value;
+  let notf = "command not found";   
+
+  output.value += "FOs-Software:~ User$ " + disp + '\n' + notf + '\n';
+
+  input.value = "";
+};
+
+function hcom() {
+  let mes = input.value;
+  let helM = "help goes here";
+
+  output.value += `FOs-Software:~ User$ ${mes} \n ${helM} \n`;
+
+  input.value = "";
+};
