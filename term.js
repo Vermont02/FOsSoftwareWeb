82a74f17c303d6c471305d52a4866290ac2677c5
Vermont02 <bquesenbm@gmail.com>
2025-03-01 18:03:37 -0500

    update js

--- term.js
+++ term.js
@@ -1,5 +1,6 @@
 let input = document.getElementById('terminput');
 let output = document.getElementById('termoutput');
+let user = "FOs-Software:~ User$ ";
 
 input.addEventListener('keydown', function(e) {
   if (e.key === "Enter") {
@@ -10,6 +11,12 @@ input.addEventListener('keydown', function(e) {
 function commands() {
   if (input.value === "--help" || input.value === "--h") {
      hcom();
+  } else if (input.value === "") {
+    noncom();
+  } else if (input.value === "--time") {
+    timecom();
+  } else if (input.value.startsWith("--loc")) {
+    loccom();
   } else {
   err();
   }
@@ -18,17 +25,56 @@ function commands() {
 function err() {
   let disp = input.value;
   let notf = "command not found";   
+  
+  notf.color = "red";
 
-  output.value += "FOs-Software:~ User$ " + disp + '\n' + notf + '\n';
+  output.value += user + disp + '\n' + notf + '\n';
 
   input.value = "";
 };
 
 function hcom() {
-  let mes = input.value;
-  let helM = "help goes here";
+  let disp = input.value;
+  let notf = "--help [-h]\nPrints this message.\n\n--time\nPrints current time.\n\n--loc [URL]\nRedirects user to selected URL\n\n";
 
-  output.value += "FOs-Software:~ User$ " + mes + '\n' + helM + '\n';
+  output.value += user + disp + '\n' + notf + '\n';
+
+  input.value = "";
+};
+
+function noncom() {
+  let disp = input.value;
+  
+  output.value += user + disp + '\n';
+
+  input.value = "";
+}; 
+
+function timecom() {
+  let disp = input.value;
+  let notf = new Date().toLocaleString();
+
+  output.value += user + '\n' + notf + '\n';
+
+  input.value = "";
+};
+
+function loccom() {
+  let disp = input.value.split(" ")[1];
+
+  output.value += user + disp + '\n' + notf + '\n';
+
+  if (disp) {
+  
+    let notf = "Redirecting..."
+  
+    window.open(disp);
+
+    output.value += user + disp + '\n' + notf + '\n';
+  } else {
+
+      output.value += user + disp + '\n' + notf + '\n' + "error: no URL specified";
+  }
 
   input.value = "";
 };
